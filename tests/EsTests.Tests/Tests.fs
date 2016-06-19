d08f5ecb1a2fab2ee1f9bc97ab30c69da4953b28
A. Prooks <aprooks@live.ru>
2016-06-19 15:44:43 +0200

    chore: scaffolded project
    Signed-off-by: A. Prooks <aprooks@live.ru>

---
+++ tests/EsTests.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module EsTests.Tests
+
+open EsTests
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
